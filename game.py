28eba0d4e6e2aea6dedfba556f39509328b1b0fd
justine-bordon <jtbordon@up.edu.ph>
2026-06-06 11:54:59 +0800

    Update game.py

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ from defaults import *
 
 
 if __name__ == '__main__':
-    model = GameModel.phase4()
+    model = GameModel.phase5()
     view = GameView(SCREEN_WIDTH, SCREEN_HEIGHT)
     controller = GameController(model, view)
     controller.start()
